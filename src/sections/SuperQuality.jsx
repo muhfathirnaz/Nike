4efa39846419d289a11104fe9a30b358c7dbd738
muhfathirnaz <122539375+muhfathirnaz@users.noreply.github.com>
2023-09-27 09:15:52 +0700

    done

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import { shoe8 } from "../assets/images"
 import Button from "../components/Button"
 
 const SuperQuality = () => {
@@ -5,11 +6,17 @@ const SuperQuality = () => {
     <section id="about-us" className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'>
 
       <div className='flex flex-1 flex-col'>
-      <h2 className="font-palanguin text-4xl capitalize font-bold lg:max-w-lg">We Provide You <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span><br /><span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+      <h2 className="font-palanguin text-4xl capitalize font-bold lg:max-w-lg">We Provide You <span className="text-coral-red">Super</span><span className="text-coral-red inline-block mt-3">Quality </span> Shoes
       </h2>
-      <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish nike arrivals, quality comfort, and innovation for your active life</p>
-      <Button label="view" />
+      <p className="mt-4 lg:max-w-lg info-text">Ensurign premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
+      <p className="mt-6 lg:max-w-lg info-text">Our dedication tio detail and excellence ensures your satisfaction</p>
+      <div className="mt-11">
+      <Button label="view details" />
+      </div>
+      </div>
 
+      <div className="flex-1 flex items-center justify-center"> 
+        <img src={shoe8} alt="shoe8" width={570} height={522} className="object-contain" />
       </div>
 
     </section>
